46e606428fb568116135b825706a44c878f7b051
Walker Hebert <walkerhebert@yahoo.com>
2022-11-22 13:34:16 -0600

    another commit

--- static/index.js
+++ static/index.js
@@ -1,9 +1,9 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
-let quotes = ["WINTER20", "WAKA10", "BRADYISNOTCOOL1", "BRADYISCOOL10", "CODYISMEH", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "I hope this makes you smile Cody"];
+let codes = ["WINTER20", "WAKA10", "BRADYISNOTCOOL1", "BRADYISCOOL10", "CODYISMEH", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "No code, sorry", "I hope this makes you smile Cody"];
 
 btn.addEventListener('click', function() {
-  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-  output.innerHTML = randomQuote;
+  let randomCode = codes[Math.floor(Math.random() * codes.length)]
+  output.innerHTML = randomCode;
 });
